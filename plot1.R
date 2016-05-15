09e885b35d58259e82284e67dd41c41b7afa63b1
Lizi Cuenen <lizicuenca@gmail.com>
2016-05-14 20:59:55 -0400

    fix plot1.R import

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 # This is for week 1 assignment of Exploratory Data Analysis
 # 
 # https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
-
+source("downloadFile.R")
 household2007 <-  getHousehold2007Data()
 
 png("plot1.png")
